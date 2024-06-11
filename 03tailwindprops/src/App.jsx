28f690efef47fb88ff1019a65f703bbea8b79b43
Aman Gautam <116288913+speedxaman@users.noreply.github.com>
2024-06-11 23:04:52 +0530

    tailwind learned

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './component/card'
+
+function App() {
+  
+
+  // let myArr = {
+  //   name:"aman",
+  //   mobile:"98745621"
+
+  // };
+  return (
+    <>
+     <h1>Tailwind Test</h1>
+     <Card channel="amanana"/>
+     <Card channel="565666556"/>
+    </>
+  )
+}
+
+export default App
